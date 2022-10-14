4f114de650180427a02784a54006671d7128d734
Bereket Desalegn Hailemariam <bailikedes@gmail.com>
2022-10-14 03:12:43 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+  * main: assigns a random number to var n
+  * then checks whether n is >0 or <0 or =0
+  * prints n with its sign 
+  * return 0 every iteration
+  */
 int main(void)
 {
 	int n;
