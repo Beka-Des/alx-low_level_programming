c061c2171c83b400147187f6c73057abaabf6469
Bereket Desalegn Hailemariam <bailikedes@gmail.com>
2022-10-24 10:22:03 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * puts2 - a function that prints every other
  * character of a string, starting with the
@@ -6,14 +7,14 @@
  *  @str: An input character
  *  Return: Nothing
  */
+
 void puts2(char *str)
 {
-	int string = 0;
+	int a = 0, b = 0;
 
-	while (str[string] != '\0')
-	{
-		_putchar(str[string]);
-		string+=2;
-	}
+	while (str[a++])
+		b++;
+	for (a = 0; a < b; a += 2)
+		_putchar(str[a]);
 	_putchar('\n');
 }
